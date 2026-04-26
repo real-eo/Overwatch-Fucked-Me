479f3d2049b6d6e8e04c90a51dee26ce5c7867a4
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-04-26 16:21:30 +0200

    refactor: Change to new codebase

--- README.md
+++
@@ -1,9 +0,0 @@
-> [!CAUTION]
-> ## OUTDATED!
-> The current release (v0.1.1) is outdated as of 2023!
-
----
-
-# Overwatch-Fucked-Me
-
-The perfect program for when you're in dire need of a better team comp
--- recognize.py
+++ recognize.py
@@ -1,27 +1,111 @@
 from keras.models import load_model
 from PIL import Image, ImageGrab, ImageOps
 import numpy as np
+from torch import ge
 
 
-def _crop_image_(img, d):
+
+# * Private functions
+def _crop_image(img, dx, dy):
     w, h = img.size
 
-    for i in range(1, h-h%d+1, d):
-        box = (0, i, 90, i+d)
-        img.crop(box).save(f"sources/ai/e{int((i-1)/62)}.png")
+    for i in range(1, int(h-h%dy+1), int(dy)):
+        box = (
+            0, 
+            i + (2 * int((i-1)/dy)),                                                    # ? We add 2 pixels for every new image we crop since there is a 2 pixel border between the images on the leaderboard
+            dx, 
+            i+dy + (2 * int((i-1)/dy))
+        )
+        img.crop(box).save(f"out/ai/e{int((i-1)/dy)}.png")
 
 
+def _determine_leaderboard_x1() -> int:
+    # * Constants
+    # List of X coordinates where the white bar starts at the different upgrade levels  
+    WHITE_BAR_STARTS_AT_X = (
+        320,                                                                            # ? X coordinate of the left edge at 0 upgrades
+        300,                                                                            # ? X coordinate of the left edge at 1 upgrade
+        270                                                                             # ? X coordinate of the left edge at 2 upgrades
+    )
+    
+    # Region of the white bar at the top of your own team's leaderboard
+    WHITE_BAR_REGION = (WHITE_BAR_STARTS_AT_X[2], 160, 665, 190)                        # ? We do max upgrades for x1 to be able to check all upgrade levels
+    WHITE_BAR_PIXEL_COLOR = (215, 219, 224)                                             # ? The color of the white bar pixels
+
+
+    # * Process
+    # Grab the image of the white bar region
+    whiteBarImage = ImageGrab.grab(WHITE_BAR_REGION)
+    currentStartX = WHITE_BAR_STARTS_AT_X[0]                                            # ? Start by assuming that there are 0 upgrades
+
+    # Check which upgrade level is active by checking the color of the pixel at the different X coordinates
+    for i, x in enumerate(WHITE_BAR_STARTS_AT_X[1:]):                                   # ? Start checking from 1 upgrade since we already assume 0 upgrades at the start
+        # Coordinates of the pixel we check
+        checkX = x - (                                                                  # ? We start by using the current X coordinate we want to check
+            WHITE_BAR_REGION[0]                                                         # ? Then we subtract the coordinate where the region starts since we are checking the pixel relative to the region
+            - 5                                                                         # ? Finally, we subtract 5 pixels to not be on the exact edge of the white bar, since the increments are pretty big, and we don't want any potential anti-aliasing to mess with our color check
+        ) 
+        checkY = 15                                                                     # ? Just half the width of the white bar region (we check the middle of the white bar for consistency - same as the "- 5" for the X coordinate)
+
+        # Get the color of the pixel at the coordinates
+        color = whiteBarImage.getpixel((checkX, checkY))
+
+        # Check if the color matches the white bar pixel color
+        if not color == WHITE_BAR_PIXEL_COLOR:
+            break                                                                       # ? If the color doesn't match, we break out of the loop since we found the current upgrade level
+        
+        # If the color matches, we update the current start X coordinate to the current X coordinate
+        currentStartX = x
+
+    # If we found an upgrade level, or finished checking all upgrade levels, we return the current start X coordinate
+    print(f"[§] Leaderboard x1 currently at: {currentStartX}")
+    return currentStartX
+
+
+
+# * Public functions
 def capture_image():
-    region = (310, 610, 1160, 920)
+    """
+    Captures the enemy leaderboard in game
+
+    ---
+
+    ### Process
+    1. Determine, at most, where the x1 of the leaderboard is based on how many upgrades are active for the most progressed person
+    2. (WIP) Determine how many players per team 
+    3. Grab screenshot based on the number of upgrades and team size
+    4. Split image into one image per enemy and save them
+    5. Save full leaderboard
+
+    """
+    # * 1) Determine, where the x1 of the leeaderboard is based on how many upgrades are active for the most progressed person
+    leaderboardX1 = _determine_leaderboard_x1()
+
+
+    # * 2) Determine how many players per team
+    # (WIP)
+
+
+    # * 3) Grab screenshot based on the number of upgrades and team size
+    # // region = (310, 610, 1160, 920)
+    region = (leaderboardX1, 595, 1100, 925)
     img = ImageGrab.grab(region)    
 
-    print("[$] Image captured")
+    print("[§] Image captured")
+    
 
-    _crop_image_(img, 62)
+    # * 4) Split image into one image per enemy and save them
+    # Character image size is: 64x64
+    # Role icon image size is: 27x64
+    # Border size is: 2x2
+    _crop_image(img, 93, 64)
 
-    img.save("sources/ai/enemyLeaderboard.png")
+
+    # * 5) Save full leaderboard
+    img.save("out/ai/enemyLeaderboard.png")
     
     return 0
+ 
  
 def recognize():
     np.set_printoptions(suppress=True)
@@ -35,7 +119,7 @@ def recognize():
     classesReturned = []
 
     for i in range(5):
-        image = Image.open(f'sources/ai/e{i}.png').convert('RGB')
+        image = Image.open(f'out/ai/e{i}.png').convert('RGB')
 
         image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
@@ -45,13 +129,13 @@ def recognize():
 
         data[0] = normalized_image_array
 
-        prediction = model.predict(data)
+        prediction = model.predict(data, verbose=0)
         index = np.argmax(prediction)
         class_name = class_names[index]
         confidence_score = prediction[0][index]
 
-        print('[$] Class:', class_name, end='')
-        print('[$] Confidence score:', confidence_score)
+        print('[§] Class:', class_name, end='')
+        print('[§] Confidence score:', confidence_score)
 
         classesReturned.append([class_name, confidence_score])
     
